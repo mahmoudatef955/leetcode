9ed9da856cfbef94b6df4701f6ddccf615d0a6d4
mahmoudatef <mahmoud.atef955@gmail.com>
2022-05-31 15:47:57 +0200

    Merge two sorted linked lists prob

--- main.py
+++ main.py
@@ -4,13 +4,17 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+from merge_two_sorted_list import Solution
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f"Hi, {name}")  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    sln = Solution()
+    print_hi(sln.mergeTwoLists())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
