b5b4ce1a920613266cc9b6781a6c343831dd1e46
mahmoudatef <mahmoud.atef955@gmail.com>
2022-08-30 10:26:51 +0200

    Solve problem 141. Linked List Cycle

--- main.py
+++ main.py
@@ -76,18 +76,50 @@ def findUnsortedSubarray(nums: List[int]) -> int:
     return high - low + 1
 
 
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+def has_cycle(head):
+    slow_pointer, fast_pointer = head, head.next
+    while fast_pointer.next != None:
+        if slow_pointer.value == fast_pointer.value:
+            return True
+        slow_pointer = slow_pointer.next
+        fast_pointer = fast_pointer.next.next
+    return False
+
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+    head.next.next.next.next.next.next = head.next.next
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+    head.next.next.next.next.next.next = head.next.next.next
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+
 if __name__ == "__main__":
     st = time.time()
-
+    main()
     # print(backspace_compare("gtc#uz#", "gtcm##uz#"))
     # print(backspace_compare("bxj##tw", "bxo#j##tw"))
     # print(backspace_compare("ab##", "c#d#"))
     # print(backspace_compare("ab##", "c#d#"))
-    print(findUnsortedSubarray([1, 2, 5, 3, 7, 10, 9, 12]))
+    # print(findUnsortedSubarray([1, 2, 5, 3, 7, 10, 9, 12]))
     # print(findUnsortedSubarray([1, 3, 2, 0, -1, 7, 10]))
     # print(findUnsortedSubarray([1, 2, 3]))
     # print(findUnsortedSubarray([1, 2, 3, 3, 3]))
-    print(findUnsortedSubarray([1, 3, 2, 2, 2]))
+    # print(findUnsortedSubarray([1, 3, 2, 2, 2]))
     et = time.time()
     elapsed_time = et - st
     # print('\nExecution time :', elapsed_time, 'seconds')
