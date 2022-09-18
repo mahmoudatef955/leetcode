6b879aa0f6ad93ba9161d75509c7f6d0c739c5dc
mahmoudatef <mahmoud.atef955@gmail.com>
2022-09-18 14:26:59 +0200

    solve Minimum Meeting Rooms problem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from ast import List
 from termios import FFDLY
+from heapq import *
 import time
 
 
@@ -27,50 +28,68 @@ def reverse_linked_list(head):
     return prev
 
 
-def intervalIntersection(firstList, secondList):
-    if len(firstList) == 0 or len(secondList) == 0:
-        return []
+def eraseOverlapIntervals(intervals) -> int:
+    intervals.sort(key=lambda x: x[0])
+    overlapCount = 0
+    i = 0
+    while i < len(intervals) - 1:
+        j = i + 1
+        while j < len(intervals) and intervals[j][0] < intervals[i][1]:
+            overlapCount += 1
+            if intervals[i][1] > intervals[j][1]:
+                i += 1
+            j += 1
+        i = j
+    return overlapCount
 
-    intersections = []
-    secondListPointer = 0
-    firstListPointer = 0
 
-    while firstListPointer < len(firstList) and secondListPointer < len(secondList):
-        x = max(firstList[firstListPointer][0], secondList[secondListPointer][0])
-        y = min(firstList[firstListPointer][1], secondList[secondListPointer][1])
-        if y >= x:
-            intersections.append([x, y])
+class Meeting:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
 
-        if y >= firstList[firstListPointer][1]:
-            firstListPointer += 1
-        if y >= secondList[secondListPointer][1]:
-            secondListPointer += 1
+    def __lt__(self, other):
+        return self.end < other.end
 
-    return intersections
+
+def min_meeting_rooms(meetings):
+    meetings.sort(key=lambda x: x.start)
+
+    minRoom = 0
+    roomsHeap = []
+
+    for meeting in meetings:
+
+        while len(roomsHeap) > 0 and meeting.start >= roomsHeap[0].end:
+            heappop(roomsHeap)
+
+        heappush(roomsHeap, meeting)
+        minRoom = max(minRoom, len(roomsHeap))
+
+    return minRoom
 
 
 def main():
-    st = time.time()
     print(
-        "Intervals Intersection: "
-        + str(intervalIntersection([[2, 3], [5, 7]], [[1, 3], [5, 6], [7, 9]]))
+        "Minimum meeting rooms required: "
+        + str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 5), Meeting(7, 9)]))
     )
-    # print(
-    #     "Intervals Intersection: "
-    #     + str(intervalIntersection([[1, 3], [5, 7], [9, 12]], [[5, 10]]))
-    # )
-    # print(
-    #     "Intervals Intersection: "
-    #     + str(
-    #         intervalIntersection(
-    #             [[0, 2], [5, 10], [13, 23], [24, 25]],
-    #             [[1, 5], [8, 12], [15, 24], [25, 26]],
-    #         )
-    #     )
-    # )
-    et = time.time()
-    elapsed_time = et - st
-    print("\nExecution time :", elapsed_time, "seconds\n")
+    print(
+        "Minimum meeting rooms required: "
+        + str(min_meeting_rooms([Meeting(6, 7), Meeting(2, 4), Meeting(8, 12)]))
+    )
+    print(
+        "Minimum meeting rooms required: "
+        + str(min_meeting_rooms([Meeting(1, 4), Meeting(2, 3), Meeting(3, 6)]))
+    )
+    print(
+        "Minimum meeting rooms required: "
+        + str(
+            min_meeting_rooms(
+                [Meeting(4, 5), Meeting(2, 3), Meeting(2, 4), Meeting(3, 5)]
+            )
+        )
+    )
 
 
 if __name__ == "__main__":
