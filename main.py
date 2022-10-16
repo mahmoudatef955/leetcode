f7ea0c09b377206024af2c604e937b7a1e9f2025
mahmoudatef <mahmoud.atef955@gmail.com>
2022-10-16 11:34:14 +0200

    Solve problem 112. Path Sum

--- main.py
+++ main.py
@@ -3,116 +3,33 @@ from __future__ import print_function
 from collections import deque
 
 
-class Node:
-    def __init__(self, value, next=None):
-        self.value = value
-        self.next = next
-
-    def print_list(self):
-        temp = self
-        while temp is not None:
-            print(str(temp.value) + " ", end="")
-            temp = temp.next
-        print()
-
-
-def reverse_linked_list(head):
-    prev = None
-    while head is not None:
-        current = head.next
-        head.next = prev
-        prev = head
-        head = current
-    return prev
-
-
-def eraseOverlapIntervals(intervals) -> int:
-    intervals.sort(key=lambda x: x[0])
-    overlapCount = 0
-    i = 0
-    while i < len(intervals) - 1:
-        j = i + 1
-        while j < len(intervals) and intervals[j][0] < intervals[i][1]:
-            overlapCount += 1
-            if intervals[i][1] > intervals[j][1]:
-                i += 1
-            j += 1
-        i = j
-    return overlapCount
-
-
-class job:
-    def __init__(self, start, end, cpu_load):
-        self.start = start
-        self.end = end
-        self.cpu_load = cpu_load
-
-    def __lt__(self, other):
-        return self.start < other.start
-
-
-class Node:
-    def __init__(self, value, next=None):
-        self.value = value
-        self.next = next
-
-    def print_list(self):
-        temp = self
-        while temp is not None:
-            print(temp.value, end=" ")
-            temp = temp.next
-        print()
-
-
-def reverse(head):
-    prev = None
-    while head is not None:
-        tmp = head.next
-        head.next = prev
-        prev = head
-        head = tmp
-
-    return prev
-
-
 class TreeNode:
-    def __init__(self, val):
+    def __init__(self, val, left=None, right=None):
         self.val = val
-        self.left, self.right = None, None
+        self.left = left
+        self.right = right
 
 
-def tree_right_view(root):
+def has_path(root, sum):
     if root is None:
-        return []
-    result = []
-    queue = deque()
-    queue.append(root)
-    while queue:
-        l_size = len(queue)
-        for i in range(l_size):
-            node = queue.popleft()
-            if node:
-                if i == l_size - 1:
-                    result.append(node)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-    return result
+        return False
+
+    if root.left is None and root.right is None and root.val == sum:
+        return True
+
+    return has_path(root.left, sum - root.val) or has_path(root.right, sum - root.val)
 
 
 def main():
+
     root = TreeNode(12)
     root.left = TreeNode(7)
     root.right = TreeNode(1)
     root.left.left = TreeNode(9)
     root.right.left = TreeNode(10)
     root.right.right = TreeNode(5)
-    root.left.left.left = TreeNode(3)
-    result = tree_right_view(root)
-    print("Tree right view: ")
-    for node in result:
-        print(str(node.val) + " ", end='')
+    print("Tree has path: " + str(has_path(root, 23)))
+    print("Tree has path: " + str(has_path(root, 16)))
 
 
 if __name__ == "__main__":
